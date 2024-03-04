051e5feea94261abc199be37b9fe4cb80b18daac
MON3EMPASHA <monemabdelmonem144@gmail.com>
2024-03-04 19:44:54 +0200

    ASSIGNMENT SUBMISSION PAGE

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import AssignmentSubmission from "./Assignment_submission_page";
+import Navbar from "./navbar";
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <AssignmentSubmission />
+
+    </div>
+  );
+}
